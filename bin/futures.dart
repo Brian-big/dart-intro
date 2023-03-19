23ddda9129deb236a34df9758a42a949d59cf8b6
Brian Barasa <briannbig@gmail.com>
2023-03-19 13:01:00 +0300

    add counter

--- bin/futures.dart
+++ bin/futures.dart
@@ -1,4 +1,5 @@
 Future<String> createOrderMessage() async {
+  print('awaiting user orders...');
   var order = await fetchUserOrder();
   return 'your order is $order';
 }
@@ -6,7 +7,18 @@ Future<String> createOrderMessage() async {
 Future<String> fetchUserOrder() =>
     Future.delayed(const Duration(seconds: 3), () => 'Large latte');
 
-Future<void> main(List<String> args) async {
+void main(List<String> args) async {
   print('fetching user orders...');
+  await countSeconds(3);
   print(await createOrderMessage());
+}
+
+Future<void> countSeconds(int s) async {
+  for (var i = 1; i <= s; i++) {
+    Future.delayed(
+        Duration(
+          seconds: i,
+        ),
+        () => print(i));
+  }
 }
